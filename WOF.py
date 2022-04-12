54c8d94543a09bc0847b21bc8d508115538e2462
Jeannine Hall <jeannine.hall1815@gmail.com>
2022-04-12 15:49:48 -0500

    pulling random word from file

--- WOF.py
+++ WOF.py
@@ -1,7 +1,7 @@
 #create wheel (contains bankrupt, lose a turn, and 17 dollar amounts)
 wheel = ['bankrupt', 'lose a turn', 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 600, 750, 800, 850, 900]
 #create word list
-word = 'test'
+word=[]
 consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 vowels = ['a','e','i','o','u']
 correctLetter = []
@@ -16,98 +16,109 @@ account3 = 0
 
 import random
 
-#f = open(r'Documents\Repos\wheel-of-fortune\word-list.txt')
-#word = f.readlines()
-#f.close
+f = open(r'Documents\Repos\wheel-of-fortune\word-list.txt')
+wordList = f.readlines()
+f.close
+
+for line in wordList:
+    word = random.choice(wordList)
+    
+
+#for line in wordList:
+    #key=str(str(line))
+    #word[key]=line
+#for key,value in word.items():
+    #exec(f'[key]=[value]')
+
 
 for char in word:
     correctLetter.append('_')
 
-    wordSolved = False
+wordSolved = False
 
-    rounds = ['round1','round2']
-    for i in rounds:
-        print(i)
+rounds = ['round1','round2']
+for i in rounds:
+    print(i)
 
-        players = ['Joe','Mary','Jill']
+    #players = ['Joe','Mary','Jill']
 
-        for i, player in enumerate(players):
-            #print(f'Player {i}: {player}')        
+    #for i, player in enumerate(players):
+        #print(f'Player {i}: {player}')        
+        
+    while not wordSolved:        
+
+    #player 1's turn
+    #ask if they want to solve, spin, or buy a vowel
             
-            while not wordSolved:        
-
-            #player 1's turn
-            #ask if they want to solve, spin, or buy a vowel
-                    
-                userInput = (input('Do you want to spin (type "s"), buy a vowel (type "v"), or solve the puzzle (type "p")?'))
-                #if spin, spins wheel
-                if userInput == 's':
-                    wedge = random.choice(wheel)
-                    #bankrupt: loseTurn and money
-                    print(wedge)
-                    if wedge == 'bankrupt':
-                        account1 -= account1
-                        print('Lose your turn and money')
-                        print(account1)
+        userInput = (input('Do you want to spin (type "s"), buy a vowel (type "v"), or solve the puzzle (type "p")?'))
+        #if spin, spins wheel
+        if userInput == 's':
+            wedge = random.choice(wheel)
+            #bankrupt: loseTurn and money
+            print(wedge)
+            if wedge == 'bankrupt':
+                account1 -= account1
+                print('Lose your turn and money')
+                print(account1)
+                continue
+            #loseTurn: lose turn but keep money
+            elif wedge == 'lose a turn':
+                account1 += 0            
+                print('Lose your turn')
+                print(account1)
+                #continue
+            else:
+                #show letters, multiply wedge by number of instances in word and add to account
+                spinInput = (input('Guess a consonant: '))
+                if spinInput in consonants:
+                    if spinInput in word:
+                        for position in range(len(word)):
+                            if word[position] == spinInput:
+                                correctLetter[position] = spinInput
+                                account1 += wedge
+                                print('You have $',account1)
+                    else:
+                        print('That letter is not in the word. Next player.')
                         continue
-                    #loseTurn: lose turn but keep money
-                    elif wedge == 'lose a turn':
-                        account1 += 0            
-                        print('Lose your turn')
-                        print(account1)
-                        #continue
-                    else:
-                        #show letters, multiply wedge by number of instances in word and add to account
-                        spinInput = (input('Guess a consonant: '))
-                        if spinInput in consonants:
-                            if spinInput in word:
-                                for position in range(len(word)):
-                                    if word[position] == spinInput:
-                                        correctLetter[position] = spinInput
-                                        account1 += wedge
-                                        print('You have $',account1)
-                            else:
-                                print('That letter is not in the word. Next player.')
-                                continue
-                        else:
-                            print('That is not a consonant.  Lose a turn.')
-                            #continue
-                        print(correctLetter)
-                        print(spinInput)
-
-                #if buy a vowel, check that account has at least $250
-                elif userInput == 'v':
-                    if account1 >= 250:
-                        account1 -= 250
-                        print('You have $',account1)
-                    else:
-                        print('You don\'t have enough money to buy a vowel.  Spin or solve.')
-                    vowelInput = (input('Guess a vowel: '))
-                    if vowelInput in vowels:
-                            if vowelInput in word:
-                                for position in range(len(word)):
-                                    if word[position] == vowelInput:
-                                        correctLetter[position] = vowelInput
-                            
-                            else:
-                                print('That letter is not in the word.')
-                                #continue
-                    else:
-                            print('That is not a vowel.  Lose a turn.')
-                            #continue
-                    print(correctLetter)
-                    print(vowelInput)
-                
-                #if solve, compare to word, if correct, round ends, if incorrect, lose turn and move to next player
-                elif userInput == 'p':
-                    wordGuess = (input('Solve the puzzle: '))
-                    if word == wordGuess:
-                        wordSolved = True
-                        print('Congratulations, you win the round!')
-                    else:
-                        #loseTurn
-                        print('That is not the word. Your turn is over.')
-                        #continue
-
                 else:
-                    print('Choose spin, buy a vowel, or solve the puzzle.')           
+                    print('That is not a consonant.  Lose a turn.')
+                    #continue
+                print(correctLetter)
+                print(spinInput)
+
+        #if buy a vowel, check that account has at least $250
+        elif userInput == 'v':
+            if account1 >= 250:
+                account1 -= 250
+                print('You have $',account1)
+            else:
+                print('You don\'t have enough money to buy a vowel.  Spin or solve.')
+            vowelInput = (input('Guess a vowel: '))
+            if vowelInput in vowels:
+                    if vowelInput in word:
+                        for position in range(len(word)):
+                            if word[position] == vowelInput:
+                                correctLetter[position] = vowelInput
+                    
+                    else:
+                        print('That letter is not in the word.')
+                        #continue
+            else:
+                    print('That is not a vowel.  Lose a turn.')
+                    #continue
+            print(correctLetter)
+            print(vowelInput)
+        
+        #if solve, compare to word, if correct, round ends, if incorrect, lose turn and move to next player
+        elif userInput == 'p':
+            wordGuess = (input('Solve the puzzle: '))
+            if word == wordGuess:
+                wordSolved = True
+                print('Congratulations, you win the round!')
+            else:
+                #loseTurn
+                print('That is not the word. Your turn is over.')
+                #continue
+
+        else:
+            print('Choose spin, buy a vowel, or solve the puzzle.')           

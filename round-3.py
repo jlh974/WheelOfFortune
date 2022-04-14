d88d70296d9eb7824d9495e8bbe7e12357f6f6bc
Jeannine Hall <jeannine.hall1815@gmail.com>
2022-04-14 10:11:23 -0500

    given letters working

--- round-3.py
+++ round-3.py
@@ -1,38 +1,63 @@
 #Determine which player has the most money
 
 #Get word
-word2 = 'stone'
-
+word=[]
+consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+vowels = ['a','e','i','o','u']
 correctLetter = []
+
+import random
+
+f = open(R'Documents\Repos\wheel-of-fortune\word-list.txt')
+wordList = f.read().splitlines()
+f.close
+
+for line in wordList:
+    word = random.choice(wordList)
+print(word)
+
+for char in word:
+    correctLetter.append('_')
+
+
+wordSolved = False
+
+while not wordSolved:        
+
 
 #Reveal R-S-T-L-N-E
 # 'r','s','t','l','n','e'
 
+        givenLetters = 'r,s,t,l,n,e'
+        slicedLetters = givenLetters.split(',')
+        for i, autoInput in enumerate(slicedLetters):
+            if autoInput in word:
+                for position in range(len(word)):
+                    if word[position] == autoInput:
+                        correctLetter[position] = autoInput
+            print(correctLetter)
 
-for char in word2:
-    correctLetter.append('_')
+        
 
-    wordSolved = False
 
-    autoInput = "rstlne"                     
-    if autoInput in word2:
-            for position in range(len(word2)):
-                if word2[position] == autoInput:
-                    correctLetter[position] = autoInput
+        #Ask player for 3 more consonants and one more vowel
+        conInput = (input('Guess 3 more consonants: '))
+        if conInput in consonants:
+                if conInput in word:
+                    for position in range(len(word)):
+                        if word[position] == conInput:
+                            correctLetter[position] = conInput
 
-    userInput = (input('Guess a letter: '))                        
-    if userInput in word2:
-            for position in range(len(word2)):
-                if word2[position] == userInput:
-                    correctLetter[position] = userInput
+                else:
+                    print('That letter is not in the word.')
+        else:
+            print('That is not a consonant.')
 
-    print(correctLetter)
 
-#Ask player for 3 more consonants and one more vowel
 
-#Reveal those letters
+        #Reveal those letters
 
-#Count down 5 seconds
+        #Count down 5 seconds
 
-#If player guesses correctly before time is up, they win
-#If not, they lose
+        #If player guesses correctly before time is up, they win
+        #If not, they lose
